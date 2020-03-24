37dac38c37948a5e5cc192972baca9a6a4341944
NathanielChavdarov <49632679+NathanielChavdarov@users.noreply.github.com>
2020-03-24 22:11:19 +0000

    Update search.py

--- search.py
+++ search.py
@@ -1,12 +1,10 @@
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
 def binarysearch(l, target):
     low = 0
     high = len(l)-1
     while low <= high:
         mid = int((low + high)/2)
         if target == l[mid]:
-            return True
+            return 1
         elif target < l[mid]:
             high = mid - 1
         else:
