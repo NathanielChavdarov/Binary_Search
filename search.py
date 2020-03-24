b670e51aea3bece013f9fae731e040f792634285
NathanielChavdarov <49632679+NathanielChavdarov@users.noreply.github.com>
2020-03-24 20:57:58 +0000

    Update search.py

--- search.py
+++ search.py
@@ -1,4 +1,3 @@
-import datetime
 
 l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -9,12 +8,9 @@ def binarysearch(l, target):
     while low <= high:
         mid = int((low + high)/2)
         if target == l[mid]:
-            print("Found")
             return True
         elif target < l[mid]:
             high = mid - 1
         else:
             low = mid + 1
-    return False
-
-binarysearch(l, 9)
+    return -1
